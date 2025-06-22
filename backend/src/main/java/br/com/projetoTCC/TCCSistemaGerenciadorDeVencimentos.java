594b56c84fd4c3dc7b549404224a945a9fc64820
Vinicius Vanderlei Santana <vinisantana1112@gmail.com>
2025-06-22 12:52:27 -0300

    Correção da pasta backend

---
+++ backend/src/main/java/br/com/projetoTCC/TCCSistemaGerenciadorDeVencimentos.java
@@ -0,0 +1,13 @@
+package br.com.projetoTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TCCSistemaGerenciadorDeVencimentos {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TCCSistemaGerenciadorDeVencimentos.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/projetoTCC/TCCSistemaGerenciadorDeVencimentosTests.java
@@ -0,0 +1,14 @@
+package br.com.projetoTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TCCSistemaGerenciadorDeVencimentosTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("teste");
+	}
+
+}
