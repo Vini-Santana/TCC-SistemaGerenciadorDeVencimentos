92af5e913e6de42cd1526e01a4dfc1b4af1384b1
Vinicius Vanderlei Santana <vinisantana1112@gmail.com>
2026-07-04 18:58:24 -0300

    Revert "Alterações para colocar no servidor"
    This reverts commit da0bdcaaa70e0dca5c7d5e12fed06a1856222775.

--- backend/src/main/java/br/com/projetoTCC/domain/exceptions/UsuarioNotFoundExceptionId.java
+++
@@ -1,8 +0,0 @@
-package br.com.projetoTCC.domain.exceptions;
-
-public class UsuarioNotFoundExceptionId extends RuntimeException {
-
-    public UsuarioNotFoundExceptionId(String usuario) {
-        super("Usuario " + usuario + " não encontrado.");
-    }
-}
